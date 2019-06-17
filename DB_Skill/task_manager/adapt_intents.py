0f7a66e57dd69ad74c2ca17471bace11a849f785
ahmedelq <ahmed.elq53@gmail.com>
2019-06-17 20:36:21 +0300

    with two intends

--- DB_Skill/task_manager/adapt_intents.py
+++ DB_Skill/task_manager/adapt_intents.py
@@ -1,41 +1,83 @@
+import json
+import sys
 from adapt.intent import IntentBuilder
+from adapt.engine import IntentDeterminationEngine
 
-add_keywords = [ # list of key words
-    'create',
-    'add',
-    'new',
-    'make'
+engine = IntentDeterminationEngine()
+
+
+
+def weatherFun():
+    print("Weather!")
+
+def helloFun():
+    print("Hello!")
+
+
+
+weather_keyword = [
+    "What",
+    "How",
+    "rig"
 ]
 
-todo_keywords = [
-    'todo',
-    'todos',
-    'task',
-    'tasks',
-    'chore',
-    'chores',
-    'errand',
-    'errands',
-    'assignment',
-    'assignments'
+
+
+for wk in weather_keyword:
+    engine.register_entity(wk, "WeatherKeyword")
+
+weather_types = [
+    "closest",
+    "sunny",
+    "wind",
+    "sleet",
+    "sun"
 ]
 
-todo_regex_keywords = ['{} (?P<Todo>.*)'.format(keyword)
-                       for keyword in todo_keywords]
+for wt in weather_types:
+    engine.register_entity(wt, "WeatherType")
 
-add_todo_intent = IntentBuilder('AddTodoIntent')\
-    .require('AddKeyword')\
-    .require('TodoKeyword')\
-    .require('Todo')\
+locations = [
+    "Seattle",
+    "San Francisco",
+    "Tokyo"
+]
+
+for loc in locations:
+    engine.register_entity(loc, "Location")
+
+weather_intent = IntentBuilder("WeatherIntent")\
+    .require("WeatherKeyword")\
+    .optionally("WeatherType")\
     .build()
 
 
-entities = {  # making a dic for entities
-  'AddKeyword': add_keywords,
-  'TodoKeyword': todo_keywords
+engine.register_intent_parser(weather_intent)
+
+hello_keyword = [
+    "hello world",
+    "hello",
+    "rig"
+]
+
+for hk in hello_keyword:
+    engine.register_entity(hk, "HelloKeyword")
+
+
+hello_intent = IntentBuilder("HelloIntent")\
+    .require("HelloKeyword")\
+    .build()
+
+engine.register_intent_parser(hello_intent)
+
+intent_dict = {
+    "HelloIntent" : helloFun,
+    "WeatherIntent" : weatherFun
 }
-multi_regex_entities = [todo_regex_keywords]
 
-intents = [add_todo_intent]  # adding intent to a new list
-
+if __name__ == "__main__":
+    for intent in engine.determine_intent(' '.join(sys.argv[1:])) :
+        if intent.get('confidence') > 0:
+            intent_dict.get(intent.get('intent_type'))()
+            print(json.dumps(intent, indent=4))
 
