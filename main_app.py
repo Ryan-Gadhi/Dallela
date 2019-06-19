296f3faaaee809aa3e3f0c16e31293c2de44b09c
Ryan-Gadhi <ryangadhi@yahoo.com>
2019-06-19 17:00:54 +0300

    trying to fix the two issues: infinite loop from pyttsx and gui termination

--- main_app.py
+++ main_app.py
@@ -1,6 +1,7 @@
-from interpeter.skills.Trvial_Skill import TrivialSkills
+from tkinter import *
 from AudioUtils import *
 from interpeter.skills.Trvial_Skill import TrivialSkills
+
 
 
 def find_skill(text):
@@ -8,24 +9,50 @@ def find_skill(text):
 	checks if the skill is DB or Trivial or DuckDuckGo
 	"""
 	skill_found = TrivialSkills.search_for_match(text)  # this will say if match found
-
+	print('finished search for trivial')
 	if (not skill_found):
 		pass
 	# search for the Question in the DB
 	#
 	#
 
+
+def drawGUI():
+	root = Tk()
+	frame = Frame(root)
+	frame.pack()
+	root.geometry("100x100")
+
+	frame1 = Frame(root)
+
+	b1 = Button(frame, text='Red', fg='red',command=program_flow)
+	b1.pack()
+	root.mainloop()
+
+
+def program_flow():
+	test_audio()
+	audio_text = 'tell me a joke'
+	find_skill(audio_text)
+
+
 """ 
 	Main code
 """
+
 
 #todo: do adapt processing
 
 # look_for_trigger()
 # audio_text = start_listening()
-test_audio()
-audio_text = 'tell me a joke'
-find_skill(audio_text)
+
+drawGUI()
+
+
+
+# test_audio()
+# audio_text = 'tell me a joke'
+# find_skill(audio_text)
 
 
 # todo: make a method that searches through skills to find the aprioperiate one (this is before nlp)
