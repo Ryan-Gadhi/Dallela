ac26467f07e8cfda3f4a0dd2615d486631681184
abdul-mohsen <46282360+abdul-mohsen@users.noreply.github.com>
2019-06-26 10:03:49 +0300

    nothing

--- interpeter/engine.py
+++ interpeter/engine.py
@@ -1,7 +1,6 @@
-import os
-
+import os 
+import json
 from adapt.engine import IntentDeterminationEngine
-
 
 class Engine:
     eng = None  # engine object (singleton)
@@ -22,17 +21,14 @@ class Engine:
         """
             Dynamically loads (imports) all the skills located in the folder Skills
         """
-
+        
         for folder in os.listdir("skills"):
             if not folder.startswith("Daleelah_"): continue # To avoid any other folder not related to the skills
-
-            # @Ryan,import a skill module and automaticly executes __init__.py
-            module = __import__("interpeter.skills." + folder, fromlist=[''])# import a skill module
-
-            # @Ryan, adding skills objects to the list and assigning functions to mapper dic. check any __init__.py
+            print("skills." + folder)
+            module = __import__("interpeter.skills." + folder, fromlist=['']) # import a skill module
             self.skills.append( getattr(module, "getSkill")() ) # getting a skill object
-
-        print(str(len(self.skills)), " skills has been loaded")
+       
+        print(str(len(self.skills)), " skills has been loaded") 
 
     def __register_eng_entities(self):
         """ 
@@ -41,13 +37,13 @@ class Engine:
         entities = {}
         reg_entities = []
         for skill in self.skills:
-            entities.update( skill.getEntities )  # @Ryan, saving entities in a dic
+            entities.update( skill.getEntities )
             reg_entities += skill.getRegexEntities
 
         for reg_entity in reg_entities:
-            self.eng.register_regex_entity(reg_entity) # @Ryan, registering regex(es) in Adapt engine
+            self.eng.register_regex_entity(reg_entity)
 
-        for entity, keywords in entities.items(): # @Ryan, keyword like: 'rigs', entity like: 'field_keyword'
+        for entity, keywords in entities.items():
             for keyword in keywords:
                 self.eng.register_entity(keyword, entity)
     
@@ -57,7 +53,7 @@ class Engine:
             Collects all handlers from Skill modules
         """
         for skill in self.skills:
-            self.handlers += skill.getMap # @Ryan, each skill's intent has a handler that is fetched
+            self.handlers += ( skill.getMap )
         print(str(len(self.handlers)), " handlers has been loaded")
 
 
@@ -111,11 +107,9 @@ class Engine:
         else:
             #TODO: handle unhandeled text, maybe search in duckduckgo or something
             print("I DO NOT UNDERSTAND")
-            return None
 
 
 if __name__ == "__main__":
     e = Engine()
+    print(e.compute("what is the company having most active rigs"))
 
-    print(e.compute("what are the active hours for baker hughes"))
-    print(e.compute("what rigs in   h"))
