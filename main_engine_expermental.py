57833a92d3469fdbf03dce77c0f1f2b6919106c3
ahmedelq <ahmed.elq53@gmail.com>
2019-06-30 14:56:23 +0300

    after merge

--- main_engine_expermental.py
+++ main_engine_expermental.py
@@ -1,14 +1,4 @@
 from interpeter.engine import Engine
 
-<<<<<<< HEAD
 eng = Engine()
 print( eng.compute("How many active rigs yesterday") )
-=======
-e = Engine()
-print(e.compute("what is the numebr of active rigs todey"))
-# print(e.compute("what is the product line "))
-
-#  print(e.compute('what is the product line in dammam wells'))
-# print(e.compute('what is the number of active rigs'))
-# what baker hughes product line is currently active in dammam
->>>>>>> c669c8c3c9b0df95a2b6e615bd2445ec4c604061
