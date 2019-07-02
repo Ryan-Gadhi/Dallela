e383214700fcc57f5ef4c7bd4f4a818397b97347
abdul-mohsen <46282360+abdul-mohsen@users.noreply.github.com>
2019-07-02 16:45:19 +0300

    added most active intent and list rigs

--- interpeter/skills/Daleelah_BH/__init__.py
+++ interpeter/skills/Daleelah_BH/__init__.py
@@ -20,21 +20,18 @@ today = date
 BigPlayerDic = {
     'baker hughes': '\'BH\'',
     'baker': '\'BH\'',  # todo: find correct short name
-    'BH' : 'baker hughes',
-    '\"BH\"' : 'baker hughes'
+    'BH': 'baker hughes',
+    '\"BH\"': 'baker hughes'
 }
 
 cityDecoder = {
-    "Dammam":'\'DMMM\'',
-    "dammam":'\'DMMM\''
+    "Dammam": '\'DMMM\'',
+    "dammam": '\'DMMM\''
 
 }
 operationDecoder = {
-    'OTH' : "Other"
+    'OTH': "Other"
 }
-
-
-
 
 
 def field_locator_intent_func(*args, **kwargs):
@@ -42,55 +39,55 @@ def field_locator_intent_func(*args, **kwargs):
     sql += ('date={today}'.format(today))
     result = sendQuery(sql)
 
-    #print("field locator intent function executed!")
-    return {'field_name':'Harad00', 'field_distance':'5km'}
+    # print("field locator intent function executed!")
+    return {'field_name': 'Harad00', 'field_distance': '5km'}
+
 
 def time_period_calc(response):
     print(response)
     monthes = {
-        'january' : 1,
-        'february' : 2,
-        'march' : 3,
-        'april' : 4,
-        'may' : 5,
-        'june' : 6,
-        'july' : 7,
-        'august' : 8,
-        'september' : 9,
-        'october' : 10,
-        'november' : 11,
-        'december' : 12
+        'january': 1,
+        'february': 2,
+        'march': 3,
+        'april': 4,
+        'may': 5,
+        'june': 6,
+        'july': 7,
+        'august': 8,
+        'september': 9,
+        'october': 10,
+        'november': 11,
+        'december': 12
     }
 
-
-
-    start_date = datetime.date.today() #today's date
+    start_date = datetime.date.today()  # today's date
     end_date = start_date + datetime.timedelta(days=1)
     answer = "today"
     if "month_kwd" in response:
         print('Month detected')
         month = monthes.get(response.get("month_kwd"))
-        year = int(response.get("n_kwd", start_date.year)) #graps the year, if not mentioned assumes it is the current year
+        year = int(
+            response.get("n_kwd", start_date.year))  # graps the year, if not mentioned assumes it is the current year
         start_date = datetime.datetime(year, month, 1)
         end_date = start_date + relativedelta(months=+1)
         answer = "in " + str(month) + " " + str(year)
 
     period = response.get('period_kwd', None)
 
-    if period: # going back in date
+    if period:  # going back in date
         # end_date = start_date
         n_period = int(response.get('n_kwd', 1))
 
     if period == 'yesterday':
         start_date = start_date - datetime.timedelta(days=1)
         answer = "yesterday"
-    elif period in ['week','weeks']:
+    elif period in ['week', 'weeks']:
         start_date = start_date - relativedelta(weeks=+n_period)
         answer = str(n_period) + " " + period + " ago"
-    elif period in ['month','months']:
+    elif period in ['month', 'months']:
         start_date = start_date - relativedelta(months=+n_period)
         answer = str(n_period) + " " + period + " ago"
-    elif period in ['year','years']:
+    elif period in ['year', 'years']:
         start_date = start_date - relativedelta(years=+n_period)
         answer = str(n_period) + " " + period + " ago"
     elif period in ['day', 'days']:
@@ -99,20 +96,17 @@ def time_period_calc(response):
 
     end_date = start_date + datetime.timedelta(days=1)
 
-
-
-
-
     print(start_date, end_date)
     return str(start_date), str(end_date), answer
 
-def production_Intent_func(*args,**kwargs):
+
+def production_Intent_func(*args, **kwargs):
     sql = 'Select (operatingHours-24) from {table} where '.format(table=table)
-    sql+= 'date = {today}'.format(today=today)
+    sql += 'date = {today}'.format(today=today)
 
-    return {'hours':'اي شي'}
+    return {'hours': 'اي شي'}
 
-    #result = sendQuery('select operating_hours from tablename where date = {date}') # loss = 24 - result
+    # result = sendQuery('select operating_hours from tablename where date = {date}') # loss = 24 - result
 
 
 def number_of_active_rigsfunc(*args, **kwargs):
@@ -122,13 +116,13 @@ def number_of_active_rigsfunc(*args, **kwargs):
     print(engine_answer)
     start_date, end_date, answer = time_period_calc(args[0])
     sql_query = \
-    "SELECT COUNT(DISTINCT well) FROM {table_name} \
+        "SELECT COUNT(DISTINCT well) FROM {table_name} \
       WHERE \"Date\" >= '{first_date}' AND \"Date\" < '{second_date}'".format(**
-      {
-          'table_name' : table,
-          'first_date' : start_date,
-          'second_date': end_date,
-      })
+                                                                                      {
+                                                                                          'table_name': table,
+                                                                                          'first_date': start_date,
+                                                                                          'second_date': end_date,
+                                                                                      })
 
     query_res = sendQuery(sql_query)
     print(sql_query)
@@ -136,12 +130,11 @@ def number_of_active_rigsfunc(*args, **kwargs):
     print("start date is: ", start_date, ", end date=", end_date)
     # date = datetime.datetime.now()  # the format of this needs to be changed
     # result = sendQuery('select count (distinct Level_0) from tablename where date = {date};'.format(date))
-	# todo: format the sql output to match the answer format
+    # todo: format the sql output to match the answer format
     count = query_res.get("rows")[0]["count"]
     # if not args[0].get("period_kwd", None):
     #     count = 201
-    return {"number_of_active_rig":count, "optional" : answer}
-
+    return {"number_of_active_rig": count, "optional": answer}
 
 
 def product_line_intent_func(*args, **kwargs):
@@ -149,65 +142,61 @@ def product_line_intent_func(*args, **kwargs):
     BigPlayer_name = args[0].get("BigPlayer", None)
     start_date, end_date, tail_answer = time_period_calc(args[0])
 
-
-    #date_1 = ' \"Date\" >= \'2019-06-01\' AND \"Date\" < \'2019-07-02\''
-    date_1 = '\"Date\" >= \'' +start_date+ '\' AND \"Date\" < \''+ end_date + '\''
+    # date_1 = ' \"Date\" >= \'2019-06-01\' AND \"Date\" < \'2019-07-02\''
+    date_1 = '\"Date\" >= \'' + start_date + '\' AND \"Date\" < \'' + end_date + '\''
     if field_name is not None:
         print(field_name + ' is field name')
     else:
-        #field_name = '\'DMMM\''  # default todo: should be changed to shortcuts only
-        field_name = 'dammam' # default for now
+        # field_name = '\'DMMM\''  # default todo: should be changed to shortcuts only
+        field_name = 'dammam'  # default for now
 
     if BigPlayer_name is not None:
         print(BigPlayer_name)
     else:
         BigPlayer_name = 'baker hughes'
 
+        entries = {'selection': 'ProductLine',
+                   'table': table,
+                   'field': field_name,
+                   'column': 'field'}
 
+        sql = 'select {selection} from {table} where {column} = {field} and '.format(**entries)
+        sql += 'date = {today}'.format(today=today)
 
-	entries = {'selection': 'ProductLine',
-				'table': table,
-				'field':field_name,
-				'column':'field'}
+        result = {'ProdLine': 'drilling'}  # todo: should be replaced with bottom 2 lines
+        # result = sendQuery(sql)
+        # result = json.loads(result)
 
-	sql = 'select {selection} from {table} where {column} = {field} and '.format(**entries)
-	sql += 'date = {today}'.format(today=today)
+        prodLine_keyword = result['ProdLine']
 
-
-	result = {'ProdLine':'drilling'} # todo: should be replaced with bottom 2 lines
-	# result = sendQuery(sql)
-	# result = json.loads(result)
-
-	prodLine_keyword = result['ProdLine']
-
-	return {'product_line':prodLine_keyword,'field_name':field_name}
+        return {'product_line': prodLine_keyword, 'field_name': field_name}
 
 
 def operating_hours_func(*args, **kwargs):
-	field_name = args[0].get("field_name", None)
-	BigPlayer = args[0].get("big_player1", None)
+    field_name = args[0].get("field_name", None)
+    BigPlayer = args[0].get("big_player1", None)
 
-	if (field_name is not None):
-		pass
-	else:
-		field_name = 'HMYM'  # default val. todo: should be changed to shortcuts only
+    if (field_name is not None):
+        pass
+    else:
+        field_name = 'HMYM'  # default val. todo: should be changed to shortcuts only
 
-	entries = {'selection': '(OperatingHours-24)',
-				'table': table,
-				'field': field_name,
-				'column': 'field',
-	           'BigPlayer': BigPlayerDic[BigPlayer]}
+    entries = {'selection': '(OperatingHours-24)',
+               'table': table,
+               'field': field_name,
+               'column': 'field',
+               'BigPlayer': BigPlayerDic[BigPlayer]}
 
-	sql = 'select {selection} from {table} where {column} = {field} and ' \
-	      '\"BigPlayer\" = {BigPlayer} and '.format(**entries)
-	sql += 'date = {today}'.format(today=today)
+    sql = 'select {selection} from {table} where {column} = {field} and ' \
+          '\"BigPlayer\" = {BigPlayer} and '.format(**entries)
+    sql += 'date = {today}'.format(today=today)
 
-	result = '15'  # todo: should be replaced with bottom 2 lines
-	# result = sendQuery(sql)
-	# result = json.loads(result)
-	time  = int(result)  # the query returns a number
-	#print(sql, ' is sql')
-	return {"time": time}
+    result = '15'  # todo: should be replaced with bottom 2 lines
+    # result = sendQuery(sql)
+    # result = json.loads(result)
+    time = int(result)  # the query returns a number
+    # print(sql, ' is sql')
+    return {"time": time}
 
 
 def most_active_func(*args, **kwargs):
@@ -255,32 +244,32 @@ mapper = {
     "NumberOfActiveRigsIntent": number_of_active_rigsfunc,
     "FieldStatusIntent": product_line_intent_func,
     "TimeOfOperationIntent": operating_hours_func,
-	"ProductionIntent":production_Intent_func
+    "ProductionIntent": production_Intent_func,
+    "RigListerIntent": list_rigs_in_filed_func,
+    "MostActiveIntent": most_active_func
 }
 
 
 def sendQuery(sql_string):
-	# api-endpoint
-	URL = 'http://localhost:3001/db'
-	#sql = "select * from oph_table_v0 limit 10"
+    # api-endpoint
+    URL = 'http://localhost:3001/db'
+    # sql = "select * from oph_table_v0 limit 10"
 
-	# defining a params dict for the parameters to be sent to the API
-	PARAMS = {'q':sql_string}
+    # defining a params dict for the parameters to be sent to the API
+    PARAMS = {'q': sql_string}
 
-	# sending get request and saving the response as response object
-	r = requests.post(URL,data=PARAMS)
-	# extracting data in json format
-	data = r.json()
+    # sending get request and saving the response as response object
+    r = requests.post(URL, data=PARAMS)
+    # extracting data in json format
+    data = r.json()
 
-	#return data
-	return {}
-
-
+    # return data
+    return {}
 
 
 # You can create a skill both with a json or manually
 
-class fieldLocatorSkill(Skill): # @Ryan, recom: having a skill passed is confusing. since it is not used
+class fieldLocatorSkill(Skill):  # @Ryan, recom: having a skill passed is confusing. since it is not used
     def __init__(self):
         super().__init__()
         # load functions from dictionary to handler
@@ -288,13 +277,13 @@ class fieldLocatorSkill(Skill): # @Ryan, recom: having a skill passed is confusi
             handler.func = mapper.get(handler.intent.name, None)  # if it has no function set None
 
 
-
 def getSkill():
     return fieldLocatorSkill()  # @Ryan, returns a skill object that was just assigned a bunch of functions
 
+
 if __name__ == '__main__':
-	print(today)
-	pass
+    print(today)
+    pass
 #
 # result = product_line_intent_func()
 # print(result)
