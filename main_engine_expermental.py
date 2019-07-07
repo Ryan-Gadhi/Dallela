45a73b43ac0f60549726fa0404c77a3bf549f72a
abdul-mohsen <46282360+abdul-mohsen@users.noreply.github.com>
2019-07-07 08:47:33 +0300

    good

--- main_engine_expermental.py
+++ main_engine_expermental.py
@@ -12,7 +12,7 @@ eng = Engine()
 # print( eng.compute("Show me the fields with the most producing wells") )
 # print( eng.compute("How many active rigs 2 weeks ago") )
 #
-# print( eng.compute("what is the efficiency for baker hughes") )
+print( eng.compute("what is the efficiency for baker hughes") )
 #
 # print( eng.compute("What is the weather for 123 in tokyo") )
 
