54f5fc4b2731ee203309cb6b7c5c6781bbff9795
Al-Majed <abdul-mohsen.al-majed@bakerhughes.com>
2019-07-14 08:41:20 +0300

    pls

--- main_engine_expermental.py
+++ main_engine_expermental.py
@@ -2,13 +2,13 @@ from interpeter.engine import Engine
 
 eng = Engine()
 
-# print( eng.compute("show the activity on the well dmmm129") )
+print( eng.compute("show the activity on the well dmmm129") )
 
 # print( eng.compute("What is the weather for 123 in tokyo") )
 #
 # print( eng.compute("How many lost hours for bh in MyRoom in April 2017") )
 
-print( eng.compute("detailed summary for QTIF_224") )
+# print( eng.compute("detailed summary for QTIF_224") )
 
 # print( eng.compute("show me the most producing fields today") )
 # print(eng.compute("many active wellbore from baker "))
